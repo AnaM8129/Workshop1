9c77a519ac17bf0476f197ce501bafafd8cf7029
dpaeza <danipaez1098@gmail.com>
2023-01-23 16:51:37 -0500

    delate db.json and add global array for icons elements

--- main.js
+++ main.js
@@ -13,6 +13,100 @@ const weightPokemon = document.getElementById("weight");
 const form = document.getElementById("form");
 const iconsContainer = document.getElementById("iconsContainer");
 let pokeFilter = "";
+const elementsIcons = [
+        {
+            id:1,
+            name:'bug',
+            icon:'./elementsIcons/bug.svg'
+        },
+        {
+            id:2,
+            name:'dark',
+            icon: './elementsIcons/dark.svg'
+        },
+        {
+            id:3,
+            name:'dragon',
+            icon:"./elementsIcons/dragon.svg"
+        },
+        {
+            id:4,
+            name:"electric",
+            icon:"./elementsIcons/electric.svg"
+        },
+        {
+            id:5,
+            name:"fairy",
+            icon:"./elementsIcons/fairy.svg"
+        },
+        {
+            id:6,
+            name:"fighting",
+            icon:"./elementsIcons/fighting.svg"
+        },
+        {
+            id:7,
+            name:"fire",
+            icon:"./elementsIcons/fire.svg"
+        },
+        {
+            id:8,
+            name:"flying",
+            icon:"./elementsIcons/flying.svg"
+        },
+        {
+            id:9,
+            name:"ghost",
+            icon:"./elementsIcons/ghost.svg"
+        },
+        {
+            id:10,
+            name:"grass",
+            icon:"./elementsIcons/grass.svg"
+        },
+        {
+            id:11,
+            name:"ground",
+            icon:"./elementsIcons/ground.svg"
+        },
+        {
+            id:12,
+            name:"ice",
+            icon:"./elementsIcons/ice.svg"
+        },
+        {
+            id:13,
+            name:"normal",
+            icon:"./elementsIcons/normal.svg"
+        },
+        {
+            id:14,
+            name:"poison",
+            icon:"./elementsIcons/poison.svg"
+        },
+        {
+            id:15,
+            name:"psychic",
+            icon:"./elementsIcons/psychic.svg"
+        },
+        {
+            id:16,
+            name:"rock",
+            icon:"./elementsIcons/rock.svg"
+        },
+        {
+            id:17,
+            name:"steel",
+            icon:"./elementsIcons/steel.svg"
+        },
+        {
+            id:18,
+            name:"water",
+            icon:"./elementsIcons/water.svg"
+        }
+    ]
+
+
 
 //OBTENER POKEMONES
 const getPokemons = async (inputValue = "") => {
@@ -50,12 +144,10 @@ const getPokemons = async (inputValue = "") => {
       abilities.forEach((abilitie) => {
         abilitiesList.push(abilitie.ability.name);
       });
-      types.forEach(async (type) => {
+      types.forEach((type) => {
         typesList.push(type.type.name);
-        const { data } = await axios.get(
-          `${URL_API_ICONS}?name=${type.type.name}`
-        );
-        iconsList.push(data[0].icon);
+        pruebas = elementsIcons.filter((poke) => poke.name.includes(type.type.name));
+        iconsList.push(pruebas[0].icon);
       });
       const newPokemon = {
         id: id,
